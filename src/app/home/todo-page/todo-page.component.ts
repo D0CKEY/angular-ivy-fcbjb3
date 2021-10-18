26086bb8c0156f72cc762d2172c57216aa12f276
D0CKEY <dockeylev@gmail.com>
2021-10-18 18:36:50 +0200

    5 files modified

--- src/app/home/todo-page/todo-page.component.ts
+++ src/app/home/todo-page/todo-page.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-page',
-  templateUrl: './todo-page.component.html',
-  styleUrls: ['./todo-page.component.scss']
+  templateUrl: '/todo-page.component.html',
+  styleUrls: ['/todo-page.component.scss']
 })
 export class TodoPageComponent implements OnInit {
 
